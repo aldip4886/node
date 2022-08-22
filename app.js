398ec4a67df6137eb0e2fab1b8327cc657764dea
aldip4886 <aldip4886@gmail.com>
2022-08-22 21:36:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const fs = require('fs')
-const port = 3000
+const port = process.env.port || 5000;
 
 
 // you will need to install via 'npm install jsonwebtoken' or in your package.json
