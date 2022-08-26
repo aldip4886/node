759d8f270084d1b9932ffb0d8a71a3949c30afce
aldip4886 <aldip4886@gmail.com>
2022-08-26 23:15:25 +0700

    ss

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const port = process.env.PORT || 53604;
 
 // you will need to install via 'npm install jsonwebtoken' or in your package.json
 
-var jwt = require("jsonwebtoken");
+// var jwt = require("jsonwebtoken");
 
-var METABASE_SITE_URL = "http://metabase4.herokuapp.com";
-var METABASE_SECRET_KEY = "f8b7e029026108a5af475da4925271fc23d8378cc996bf10d114765880a44565";
+// var METABASE_SITE_URL = "http://metabase4.herokuapp.com";
+// var METABASE_SECRET_KEY = "f8b7e029026108a5af475da4925271fc23d8378cc996bf10d114765880a44565";
 
-var payload = {
-  resource: { question: 116 },
-  params: {},
-  exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
-};
-var token = jwt.sign(payload, METABASE_SECRET_KEY);
+// var payload = {
+//   resource: { question: 116 },
+//   params: {},
+//   exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
+// };
+// var token = jwt.sign(payload, METABASE_SECRET_KEY);
 
-var iframeUrl = METABASE_SITE_URL + "/embed/question/" + token + "#bordered=false&titled=false";
+// var iframeUrl = METABASE_SITE_URL + "/embed/question/" + token + "#bordered=false&titled=false";
 
 
 const server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'texthtml'})
-    fs.readFile('home.html', function(error, data) {
+    fs.readFile('index.html', function(error, data) {
         if (error){
             res.writeHead(404)
             res.write('Error: File Not Found')
